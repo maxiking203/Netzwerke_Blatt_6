d0773964891ff6da08c073fdc9869c10cbff0fca
maxiking203 <Maximilian.Sachmann@gmx.de>
2017-12-02 17:37:20 +0100

    TCP

--- src/client/Client.java
+++ src/client/Client.java
@@ -1,47 +1,85 @@
 package client;
 
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
 import java.util.Date;
 
 public class Client {
 	
+	private final static int PORT = 80;
 	private final static int N = 100;
-
+	private final static byte[] BUFFER = new byte[1400];
 	
 	public static void main(String[] args) {
-		try(DatagramSocket datagramSocket = new DatagramSocket();) {
-			
-
-		byte[] buffer = new byte[1400];
-		InetAddress receiverAddress = InetAddress.getByName("87.159.59.105");
-
-		DatagramPacket packet = new DatagramPacket(
-		        buffer, buffer.length, receiverAddress, 80);
-		int n = 1;
-		long starttime = new Date().getTime();
-		while (n <= N) {
-			datagramSocket.send(packet);
-			System.out.println("Sended package with the number:" + n);
-			
-			if(n % 10 == 0) {
-				Thread.sleep(500);
-				System.out.println("verzögert");
+		InetAddress receiverAddress = null;
+//		try {
+//			receiverAddress = InetAddress.getByName("87.159.59.105");
+//		} catch (UnknownHostException e1) {
+//			System.out.println("Wrong Host!");
+//			e1.printStackTrace();
+//		}
+//		
+//		try(DatagramSocket datagramSocket = new DatagramSocket();) {
+//
+//		DatagramPacket packet = new DatagramPacket(
+//		        BUFFER, BUFFER.length, receiverAddress, PORT);
+//		int n = 1;
+//		long starttime = new Date().getTime();
+//		while (n <= N) {
+//			datagramSocket.send(packet);
+//			System.out.println("Sended package with the number:" + n);
+//			
+//			if(n % 10 == 0) {
+//				Thread.sleep(500);
+//				System.out.println("verzögert");
+//			}
+//			n++;
+//		}
+//		long endtime = new Date().getTime();
+//		calculate(starttime, endtime);
+//		} catch (IOException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		} catch (InterruptedException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+		
+		try(Socket socket = new Socket(receiverAddress, PORT)) {
+			receiverAddress = InetAddress.getByName("localhost");
+			DataOutputStream dOut = new DataOutputStream(socket.getOutputStream());
+			int n = 1;
+			long starttime = new Date().getTime();
+			while (n <= N) {
+				dOut.writeInt(BUFFER.length);
+				dOut.write(BUFFER); 
+				System.out.println("Sended package with the number:" + n);
+				
+				if(n % 10 == 0) {
+					Thread.sleep(500);
+					System.out.println("verzögert");
+				}
+				n++;
 			}
-			n++;
-		}
-		long endtime = new Date().getTime();
-		long rate = (((1400*N)*8)/1000)/((endtime - starttime)/1000);
-		System.out.println("Senderate: " + rate);
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
+			System.out.println("Connection failed or lost");
 			e.printStackTrace();
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+				
+				
 
+	}
+	
+	private static void calculate(long start, long end) {
+		long rate = (((1400*N)*8)/1000)/((end - start)/1000);
+		System.out.println("Senderate: " + rate + " kbit/s");
 	}
 }
--- src/server/Server.java
+++ src/server/Server.java
@@ -1,51 +1,74 @@
 package server;
 
+import java.io.DataInputStream;
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.net.SocketException;
 import java.net.SocketTimeoutException;
 import java.util.Date;
 
 public class Server {
 	
-	private final static int N = 1000;
+	private final static int PORT = 80;
+	private final static int N = 100;
+	private final static byte[] BUFFER = new byte[1400];
 	
 	@SuppressWarnings("resource")
 	public static void main(String[] args) {
 		int packageC = 0;
 		int failuers = 0;
 		long starttime = new Date().getTime();
-		try (DatagramSocket datagramSocket = new DatagramSocket(80);) {
-			datagramSocket.setSoTimeout(5000);
+//		try (DatagramSocket datagramSocket = new DatagramSocket(PORT);) {
+//			datagramSocket.setSoTimeout(5000);
+//			DatagramPacket packet = new DatagramPacket(BUFFER, BUFFER.length);
+//
+//			for(int n = 1; n <= N; n++) {
+//			try {
+//			datagramSocket.receive(packet);
+//			packageC++;
+//			System.out.println("got package:" + packageC);
+//			}
+//            catch (SocketTimeoutException e) {
+//                // timeout exception.
+//                System.out.println("Timeout reached!!! " + e);
+//                failuers++;
+//                
+//            }
+//		}
+//		long endtime = new Date().getTime();
+//		calculate(starttime, endtime);
+//		} catch (IOException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		
+//		}
+//		System.out.println("revieved: " + packageC);
+//		System.out.println("missed: " + failuers);
+		
+		////////////
+		
+		try(ServerSocket server = new ServerSocket(PORT)) {
+			Socket socket = server.accept();
+			DataInputStream dIn = new DataInputStream(socket.getInputStream());
 
-			byte[] buffer = new byte[1400];
-			DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
-
-			for(int n = 1; n <= N; n++) {
-			try {
-			datagramSocket.receive(packet);
-			packageC++;
-			System.out.println("got package:" + packageC);
+			int length = dIn.readInt();                    // read length of incoming message
+			if(length>0) {
+			    dIn.readFully(BUFFER, 0, BUFFER.length); // read the message
+			    System.out.println("got Package");
 			}
-            catch (SocketTimeoutException e) {
-                // timeout exception.
-                System.out.println("Timeout reached!!! " + e);
-                failuers++;
-                
-            }
-		}
-		long endtime = new Date().getTime();
-		long rate = (((1400*N)*8)/1000)/((endtime - starttime)/1000);
-		System.out.println("Empfangsraterate: " + rate);
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		
 		}
-		System.out.println("revieved: " + packageC);
-		System.out.println("missed: " + failuers);
+	}
+	
+	private static void calculate(long start, long end) {
+		long rate = (((1400*N)*8)/1000)/((end - start)/1000);
+		System.out.println("Empfangsrate: " + rate + " kbit/s");
 	}
 
 
