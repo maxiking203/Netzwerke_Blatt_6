8a6ee82a9ca6b7a0d95d2f59352b371cafc3a81e
maxiking203 <Maximilian.Sachmann@gmx.de>
2017-12-03 13:19:09 +0100

    added Konsolen eingabe

--- src/client/Client.java
+++ src/client/Client.java
@@ -8,11 +8,11 @@ import java.net.InetAddress;
 import java.net.Socket;
 import java.net.UnknownHostException;
 import java.util.Date;
+import java.util.Scanner;
 
 public class Client {
 	
 	private final static int PORT = 8080;
-	private final static int N = 2055;
 	private final static byte[] BUFFER = new byte[1400];
 	private static InetAddress receiverAddress;
 	private static long starttime;
@@ -20,76 +20,100 @@ public class Client {
 	
 	public static void main(String[] args) {
 		
+		//Einlesen der ParameterUDP
+	
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Use UDP or TCP?");
+		String protocol = scanner.nextLine();
+		System.out.println("insert IP of the Server:");
+		String ip = scanner.nextLine();
+		System.out.println("Enter Package amount:");
+		int amount = Integer.parseInt(scanner.nextLine());
+		System.out.println("insert n (Number of Package after which the client will sleep):");
+		int n = Integer.parseInt(scanner.nextLine());
+		System.out.println("insert k (time in ms which the client will slepp):");
+		int k = Integer.parseInt(scanner.nextLine());
+		/////
 		
+		/////SET HOST Ip/////
 		try {
-			receiverAddress = InetAddress.getByName("localhost");
+			receiverAddress = InetAddress.getByName(ip);
 			System.out.println("SERVER IP SET");
 		} catch (UnknownHostException e1) {
 			System.out.println("Wrong Host!");
 			e1.printStackTrace();
 		}
+		/////
 		
-		System.out.println("//******UDP*****//");
-		try(DatagramSocket datagramSocket = new DatagramSocket();) {
-		DatagramPacket packet = new DatagramPacket(
-		        BUFFER, BUFFER.length, receiverAddress, PORT);
-		int n = 1;
-		starttime = new Date().getTime();
-		while (n <= N) {
-			datagramSocket.send(packet);
-			System.out.println("Sended package with the number:" + n);
-			
-			if(n % 150 == 0) {
-				Thread.sleep(100);
-				System.out.println("verzögert");
-			}
-			n++;
-		}
-		endtime = new Date().getTime();
-		calculate(starttime, endtime);
-		datagramSocket.close();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		System.out.println("//*****UDP_END*****//");
 		
-		System.out.println("//******TCP*****//");
-		try(Socket socket = new Socket(receiverAddress, PORT)) {
-			DataOutputStream dOut = new DataOutputStream(socket.getOutputStream());
-			int n = 1;
+		////UDP////
+		if(protocol.equals("UDP") || protocol.equals("udp") || protocol.equals("UDP")) {
+			System.out.println("//******UDP*****//");
+			try(DatagramSocket datagramSocket = new DatagramSocket();) {
+			DatagramPacket packet = new DatagramPacket(
+			        BUFFER, BUFFER.length, receiverAddress, PORT);
+			int i = 1;
 			starttime = new Date().getTime();
-			while (n <= N) {
-				dOut.writeInt(BUFFER.length);
-				dOut.write(BUFFER); 
-				System.out.println("Sended package with the number:" + n);
+			while (i <= amount) {
+				datagramSocket.send(packet);
+				System.out.println("Sended package with the number:" + i);
 				
-				if(n % 10 == 0) {
-					Thread.sleep(4000);
+				if(i % n == 0) {
+					Thread.sleep(k);
 					System.out.println("verzögert");
 				}
-				n++;
+				i++;
 			}
 			endtime = new Date().getTime();
-			calculate(starttime, endtime);
-		} catch (IOException e) {
-			System.out.println("Connection failed or lost");
-			e.printStackTrace();
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			calculate(starttime, endtime, amount);
+			datagramSocket.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			System.out.println("//*****UDP_END*****//");
 		}
-		System.out.println("//*****TCP_END*****//");
+		
+		
+		/////TCP/////
+		if(protocol.equals("TCP") || protocol.equals("tcp") || protocol.equals("Tcp")) {
+			System.out.println("//******TCP*****//");
+			try(Socket socket = new Socket(receiverAddress, PORT)) {
+				DataOutputStream dOut = new DataOutputStream(socket.getOutputStream());
+				int i = 1;
+				starttime = new Date().getTime();
+				while (i <= amount) {
+					dOut.writeInt(BUFFER.length);
+					dOut.write(BUFFER); 
+					System.out.println("Sended package with the number:" + i);
+					
+					if(i % n == 0) {
+						Thread.sleep(k);
+						System.out.println("verzögert");
+					}
+					i++;
+				}
+				endtime = new Date().getTime();
+				calculate(starttime, endtime, amount);
+			} catch (IOException e) {
+				System.out.println("Connection failed or lost");
+				e.printStackTrace();
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			System.out.println("//*****TCP_END*****//");
+		}
 				
 				
 
 	}
 	
-	private static void calculate(long start, long end) {
-		long rate = (((1400*N)*8)/1000)/((end - start)/1000);
-		System.out.println("Senderate: " + rate + " kbit/s");
+	private static void calculate(long start, long end , int amount) {
+		long rate = (((1400*amount)*8)/1000)/((end - start)/1000);
+		System.out.println("Empfangsrate: " + rate + " kbit/s");
 	}
 }
--- src/server/Server.java
+++ src/server/Server.java
@@ -10,84 +10,98 @@ import java.net.Socket;
 import java.net.SocketException;
 import java.net.SocketTimeoutException;
 import java.util.Date;
+import java.util.Scanner;
 
 public class Server {
 	
-	private final static int PORT = 80;
-	private final static int N = 2055;
+	private final static int PORT = 8080;
 	private final static byte[] BUFFER = new byte[1400];
 	private static long starttime;
 	private static long endtime;
+	private final static int TIMEOUT = 5000;
 	
 	@SuppressWarnings("resource")
 	public static void main(String[] args) {
+		
+		//Einlesen der Parameter
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Use UDP or TCP?");
+		String protocol = scanner.nextLine();
+		System.out.println("Enter Package amount:");
+		int amount = Integer.parseInt(scanner.nextLine());
+		////
+		
+		/////UDP/////
+		if(protocol.equals("UDP") || protocol.equals("udp") || protocol.equals("UDP")) {
 		System.out.println("//******UDP*****//");
-		int packageC = 0;
-		int failuers = 0;
-		try (DatagramSocket datagramSocket = new DatagramSocket(PORT);) {
-			datagramSocket.setSoTimeout(5000);
-			DatagramPacket packet = new DatagramPacket(BUFFER, BUFFER.length);
-			starttime = new Date().getTime();
-			for(int n = 1; n <= N; n++) {
-			try {
-			datagramSocket.receive(packet);
-			packageC++;
-			System.out.println("got package:" + packageC);
+			int packageC = 0;
+			int failuers = 0;
+			try (DatagramSocket datagramSocket = new DatagramSocket(PORT);) {
+				datagramSocket.setSoTimeout(TIMEOUT);
+				DatagramPacket packet = new DatagramPacket(BUFFER, BUFFER.length);
+				starttime = new Date().getTime();
+				for(int i = 1; i <= amount; i++) {
+				try {
+				datagramSocket.receive(packet);
+				packageC++;
+				System.out.println("got package:" + packageC);
+				}
+	            catch (SocketTimeoutException e) {
+	                // timeout exception.
+	                System.out.println("Timeout reached!!! " + e);
+	                failuers++;          
+	            }
 			}
-            catch (SocketTimeoutException e) {
-                // timeout exception.
-                System.out.println("Timeout reached!!! " + e);
-                failuers++;          
-            }
+			long endtime = new Date().getTime();
+			calculate(starttime, endtime, amount);
+			datagramSocket.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			
+			}
+			System.out.println("revieved: " + packageC);
+			System.out.println("missed: " + failuers);
+			
+			System.out.println("//*****UDP_END*****//");
 		}
-		long endtime = new Date().getTime();
-		calculate(starttime, endtime);
-		datagramSocket.close();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
 		
-		}
-		System.out.println("revieved: " + packageC);
-		System.out.println("missed: " + failuers);
-		
-		System.out.println("//*****UDP_END*****//");
 		
 		////TCP////
-		
-		System.out.println("//******TCP*****//");
-		
-		try(ServerSocket server = new ServerSocket(PORT)) {
-			Socket socket = server.accept();
-			socket.setSoTimeout(2000);;
-			DataInputStream dIn = new DataInputStream(socket.getInputStream());
-			starttime = new Date().getTime();
-			for(int n = 1; n <= N; n++) {
-				try {
-				int length = dIn.readInt();                    // read length of incoming message
-				if(length>0) {
-				    dIn.readFully(BUFFER, 0, BUFFER.length); // read the message
-				    System.out.println("got Package " + n);
+		if(protocol.equals("TCP") || protocol.equals("tcp") || protocol.equals("Tcp")) {
+			System.out.println("//******TCP*****//");
+			try(ServerSocket server = new ServerSocket(PORT)) {
+				Socket socket = server.accept();
+				socket.setSoTimeout(TIMEOUT);
+				DataInputStream dIn = new DataInputStream(socket.getInputStream());
+				starttime = new Date().getTime();
+				for(int i = 1; i <= amount; i++) {
+					try {
+					int length = dIn.readInt();                    // read length of incoming message
+					if(length>0) {
+					    dIn.readFully(BUFFER, 0, BUFFER.length); // read the message
+					    System.out.println("got Package " + i);
+					}
+					}
+					catch(SocketTimeoutException s) {
+		                System.out.println("Timeout reached!!! " + s);
+					}
 				}
-				}
-				catch(SocketTimeoutException s) {
-	                System.out.println("Timeout reached!!! " + s);
-				}
+				endtime = new Date().getTime();
+				calculate(starttime, endtime, amount);
+				System.out.println("Connection closed");
+				socket.close();
+				server.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
 			}
-			endtime = new Date().getTime();
-			calculate(starttime, endtime);
-			System.out.println("Connection closed");
-			socket.close();
-			server.close();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			System.out.println("//*****TCP_END*****//");
 		}
-		System.out.println("//*****TCP_END*****//");
 	}
 	
-	private static void calculate(long start, long end) {
-		long rate = (((1400*N)*8)/1000)/((end - start)/1000);
+	private static void calculate(long start, long end, int amount) {
+		long rate = (((1400*amount)*8)/1000)/((end - start)/1000);
 		System.out.println("Empfangsrate: " + rate + " kbit/s");
 	}
 
